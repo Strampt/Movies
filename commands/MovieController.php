79602d5e83e3736ba62f1f6595f2b7897192860e
Francisco Azevedo <franck_extrela@hotmail.com>
2019-07-15 13:13:38 +0100

    command action para inserir na bd os filmes

--- commands/MovieController.php
+++ commands/MovieController.php
@@ -9,24 +9,34 @@ class MovieController extends Controller
 {
     /**
      * Este comando é para inserir os dados da omdb na bd
+     * @param string $filme
+     * 
      * 
      */
-    public function actionInsertMovie ($filme) 
+    public function actionInsertSearchMovie ($filme) 
     {
         $urlMovies = 'http://www.omdbapi.com/?s='.$filme.'&page=1&apikey=a782fdd1';
         $jsonMovies = file_get_contents($urlMovies);
         $dataMovies = json_decode($jsonMovies, true);
-        //foreach($dataMovies as $movie) {
-        $imbdbidMovie = $dataMovies['Search']['0']['imdbID'];
-        $titleMovie = $dataMovies['Search']['0']['Title'];
-        $yearMovie = $dataMovies['Search']['0']['Year'];
-        $posterMovie = $dataMovies['Search']['0']['Poster'];
-        Yii::$app->db->createCommand()->insert('movie', [
-            'imbdid' => $imbdbidMovie,
-            'title' => $titleMovie,
-            'year' => $yearMovie,
-            'poster' => $posterMovie,
-        ])->execute();
-        //}
+        $datalength = count($dataMovies['Search']);
+        for($i = 0; $i < 9 && $i < $datalength; $i++) {
+            $imbdbidMovie = $dataMovies['Search'][$i]['imdbID'];
+            $titleMovie = $dataMovies['Search'][$i]['Title'];
+            $yearMovie = $dataMovies['Search'][$i]['Year'];
+            $typeMovie = $dataMovies['Search'][$i]['Type'];
+            $posterMovie = $dataMovies['Search'][$i]['Poster'];
+            Yii::$app->db->createCommand()->insert('search_movie', [
+                'imdbid' => $imbdbidMovie,
+                'title' => $titleMovie,
+                'year' => $yearMovie,
+                'type' => $typeMovie,
+                'poster' => $posterMovie,
+            ])->execute();
+        }
     }
+    
+    /*public function actionInsertTitleMovie($filme,$year) 
+    {
+
+    }*/
 }
