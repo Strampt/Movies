90f532663ecdc2e5f694f8c25906ba302fe76cab
Francisco Azevedo <franck_extrela@hotmail.com>
2019-07-22 17:58:54 +0100

    file para criar a pagina(bonita) para os filmes

---
+++ views/search-movie/_filmes.php
@@ -0,0 +1,10 @@
+<?php
+use yii\helpers\Html;
+?>
+<?php
+// criar uma view com isto em baixo e aqui meto a render a view criada, 
+foreach($movies as $movie) {
+    echo Html::a($movie->title, ['/search-movie/view', 'id'=>$movie['id']]);
+    echo $movie->year; 
+}    
+?>
