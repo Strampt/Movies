b2829d73ad8f2038a7c78a2772f41df168d5d1b9
Francisco Azevedo <franck_extrela@hotmail.com>
2019-07-22 17:57:39 +0100

    o botão create só aparece para quem é admin, e render _search e _filme

--- views/search-movie/index.php
+++ views/search-movie/index.php
@@ -15,29 +15,31 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Create Search Movie'), ['create'], ['class' => 'btn btn-success']) ?>
+        <?php 
+        if(!Yii::$app->user->isGuest) {
+            echo Html::a(Yii::t('app', 'Create Search Movie'), ['create'], ['class' => 'btn btn-success']);
+        } ?>
     </p>
 
     <?php Pjax::begin(); ?>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
+    <?php /*GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
+            //['class' => 'yii\grid\SerialColumn'],
+            //'id',
             'imdbid',
             'title',
             'year',
-            'type',
-            //'poster',
-
-            ['class' => 'yii\grid\ActionColumn'],
+            //'type',
+            'poster',
+            //['class' => 'yii\grid\ActionColumn'],
         ],
-    ]); ?>
+    ]); */?>
 
+    <?php echo $this->render('_filmes', ['movies' => $movies]); ?>
     <?php Pjax::end(); ?>
 
 </div>
