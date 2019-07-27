b3fe0fc058b4573fc16ec32b189dd4c6604384fb
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-07-27 16:03:20 +0100

    configuração da ListView, assim já tenho row por x items definidos

--- views/search-movie/index.php
+++ views/search-movie/index.php
@@ -4,6 +4,9 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
 use yii\widgets\ListView;
+use yii\widgets\LinkPager;
+use yii\data\ActiveDataProvider;
+use app\models\SearchMovie;
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\SearchMovieSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -26,7 +29,8 @@ $this->title = Yii::t('app', 'Search Movies');
     <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <!-- EUREKA -->
-    <?php if($dataProvider->totalCount == 0 && strlen($searchModel->title) > 3) {
+    <?php 
+    if($dataProvider->totalCount == 0 && strlen($searchModel->title) > 3) {
         //preciso de chamar o metodo que está nos comands para ir buscar o filme :D
         echo Html::tag('h2', $searchModel->title);
     } ?>
@@ -46,14 +50,41 @@ $this->title = Yii::t('app', 'Search Movies');
         ],
     ]); */?>
 
-    <?= ListView::widget([
+    <?php
+    $colsCount = 4;
+    echo ListView::widget([
         'dataProvider' => $dataProvider,
-        'layout' => "<div class\"items\">{items}</div>",
+        'options' => [
+            'tag' => 'div',
+            'class' => 'list-wrapper',
+            'id' => 'list-wrapper',
+        ],
+        'beforeItem' => function ($model, $key, $index, $widget) use ($colsCount) {
+            if ($index % $colsCount === 0) {
+                return "<div class='row'>";
+            }
+        },
+        'layout' => "{items}\n{pager}",
+        'afterItem' => function ($model, $key, $index, $widget) use ($colsCount) {
+            $content = '';
+            if (($index > 0) && ($index % $colsCount === $colsCount - 1)) {
+                $content .= "</div>";
+            }
+            return $content;
+        },
         'itemView' => '_filmes',
-        /*'emptyTextOptions' => [
-            'class' => 'empty',
-        ],*/
+        'pager' => [
+            'firstPageLabel' => 'First',
+            'lastPageLabel' => 'Last',
+            'maxButtonCount' => 4,
+            'options' => [
+                'class' => 'pagination col-xs-12'
+            ],
+        ],
     ]);
+    if ($dataProvider->count % $colsCount !== 0) {
+        echo "</div>";
+    }
     ?>
     <?php Pjax::end(); ?>
 
