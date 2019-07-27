2aa18205ab43d3104bb10105f0b0845648f6633e
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-07-27 16:04:29 +0100

    add styles para ter a imagem centrada

--- views/search-movie/_filmes.php
+++ views/search-movie/_filmes.php
@@ -5,9 +5,9 @@ use app\models\SearchMovie;
 use yii\helpers\Url;
 ?>
 
-<div class="col-sm-4 col-xs-12">
+<div class="col-sm-3 col-xs-12">
     <div class="filmes">
-        <a href="<?=Url::to(['/search-movie/view', 'id'=>$model->id])?>"><h2><?=$model->title?></h2></a>
-        <img src="<?=$model->poster?>">
+        <a href="<?=Url::to(['/search-movie/view', 'id'=>$model->id])?>"><h5><?=$model->title?></h5></a>
+        <img id="poster" src="<?=$model->poster?>" style="width: 60%; display:block; margin:auto;">
     </div>
 </div>
