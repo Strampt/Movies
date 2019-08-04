a88a7cb02f53159395b7bc32218222a4bc2f723b
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-08-04 17:03:50 +0100

    style retirado no html, e add class para '<a>'

--- views/search-movie/_filmes.php
+++ views/search-movie/_filmes.php
@@ -6,7 +6,7 @@ use yii\helpers\Url;
 
 <div class="col-sm-3 col-xs-12">
     <div class="filmes">
-        <a href="<?=Url::to(['/search-movie/view', 'id'=>$model->id])?>"><h5><?=$model->title?></h5></a>
-        <img id="poster" src="<?=$model->poster?>" style="width: 60%; display:block; margin:auto;">
+        <a class="overlay" href="<?=Url::to(['/search-movie/view', 'id'=>$model->id])?>"><h5><?=$model->title?></h5></a>
+        <img id="poster" src="<?=$model->poster?>">
     </div>
 </div>
