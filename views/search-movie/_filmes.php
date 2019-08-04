f6534929efe6518c0c3828e747a6fb58e49f6c83
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-08-05 00:04:36 +0100

    div para ter duas imagens quando em dimensão de telemovel

--- views/search-movie/_filmes.php
+++ views/search-movie/_filmes.php
@@ -4,7 +4,7 @@ use app\models\SearchMovie;
 use yii\helpers\Url;
 ?>
 
-<div class="col-sm-3 col-xs-12">
+<div class="col-sm-3 col-xs-6">
     <div class="filmes">
         <a class="overlay" href="<?=Url::to(['/search-movie/view', 'id'=>$model->id])?>"><h5><?=$model->title?></h5></a>
         <img id="poster" src="<?=$model->poster?>">
