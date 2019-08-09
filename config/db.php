cc5074ee5bcfa7ea368bb712385af34a9b1110c9
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-08-09 10:17:32 +0100

    modificição para usar em linux

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=movie',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'stram',
+    'password' => '1234',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
