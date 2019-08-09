264bd919eb16a5a7c0eaa0194625a4912a00e04d
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-08-10 00:25:10 +0100

    common permissions em vez de safeup para up, e fiz fresh na db

--- migrations/m190809_231345_insert_common_permissions.php
+++ migrations/m190809_231345_insert_common_permissions.php
@@ -4,41 +4,36 @@ use yii\db\Migration;
 use webvimark\modules\UserManagement\models\rbacDB\Permission;
 
 /**
- * Class m190715_113454_insert_common_permissions
+ * Class m190809_231345_insert_common_permissions
  */
-class m190715_113454_insert_common_permissions extends Migration
+class m190809_231345_insert_common_permissions extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
+    public function up()
     {
         Permission::assignRoutes(\Yii::$app->getModule('user-management')->commonPermissionName, ['/site/index']);
         Permission::assignRoutes(\Yii::$app->getModule('user-management')->commonPermissionName, ['/site/error']);
         Permission::assignRoutes(\Yii::$app->getModule('user-management')->commonPermissionName, ['/search-movie/index']);
         Permission::assignRoutes(\Yii::$app->getModule('user-management')->commonPermissionName, ['/title-movie/index']);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
-        echo "m190715_113454_insert_common_permissions cannot be reverted.\n";
-
-        return false;
-    }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
 
     }
 
     public function down()
     {
-        echo "m190715_113454_insert_common_permissions cannot be reverted.\n";
+        echo "m190809_231345_insert_common_permissions cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use Safeup()/Safedown() to run migration code within a transaction.
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m190809_231345_insert_common_permissions cannot be reverted.\n";
 
         return false;
     }
