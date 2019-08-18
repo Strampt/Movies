bd04bb85bdbfa3ac61387588936c7dc17748ed88
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-08-18 16:03:04 +0100

    url manager ativo

--- config/web.php
+++ config/web.php
@@ -48,14 +48,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
     'modules'=>[
