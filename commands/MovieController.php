40ce0ecdc360479d54fb6f1e3cbc2c0d8702aa64
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-08-29 23:23:41 +0100

    comando para a consola de ir buscar os filmes, tanto searchMovie como titleMovie

--- commands/MovieController.php
+++ commands/MovieController.php
@@ -16,6 +16,7 @@ class MovieController extends Controller
      */
     public function actionInsertSearchMovie ($filme) 
     {
+        $filme = str_replace(" ", "+", $filme);
         $urlSMovies = 'http://www.omdbapi.com/?s='.$filme.'&page=1&apikey=a782fdd1';
         $jsonSMovies = file_get_contents($urlSMovies);
         $dataSMovies = json_decode($jsonSMovies, true);
@@ -26,6 +27,7 @@ class MovieController extends Controller
             $yearSMovie = $dataSMovies['Search'][$i]['Year'];
             $typeSMovie = $dataSMovies['Search'][$i]['Type'];
             $posterSMovie = $dataSMovies['Search'][$i]['Poster'];
+            MovieController::actionInsertTitleMovie($imbdbidSMovie);
             Yii::$app->db->createCommand()->insert('search_movie', [
                 'imdbid' => $imbdbidSMovie,
                 'title' => $titleSMovie,
@@ -36,10 +38,9 @@ class MovieController extends Controller
         }
     }
     
-    public function actionInsertTitleMovie($filme, $year) 
+    private function actionInsertTitleMovie($id) 
     {
-        $filme = str_replace(" ", "+", $filme);
-        $urlTMovies = 'http://www.omdbapi.com/?t='.$filme.'&y='.$year.'&plot=full&apikey=a782fdd1';
+        $urlTMovies = 'http://www.omdbapi.com/?i='.$id.'&plot=full&apikey=a782fdd1';
         $jsonTMovies = file_get_contents($urlTMovies);
         $dataTMovies = json_decode($jsonTMovies, true);
         $imdbidTMovie = $dataTMovies['imdbID'];
